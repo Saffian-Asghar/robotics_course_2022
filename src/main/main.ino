4c5a66d2e1e08d1c97df0dd3fd7a167d8e752c6c
emilioes <emil01155@gmail.com>
2022-12-29 15:21:47 -0600

    OrientationCode

--- src/main/main.ino
+++ src/main/main.ino
@@ -10,19 +10,26 @@
 #define TURN_RIGHT_DURATION     246 
 
 
+// We need to keep track of where our bot is facing
+char orientation;
+
+ZumoMotors motors;
+
+
+/**
+  Arduino default setup function
+*/
 void setup(){  
+
 }
 
+// Arduino default loop function.
 void loop()
 {
-  // put your main code here, to run repeatedly:
-    turnLeft();
-    delay(1000);
-    turnRight();
-    delay(1000);
-    goForward();
-    delay(1000);
+
 }
+
+
 
 // Functions move the bot
 /*
@@ -58,7 +65,12 @@ This function makes the bot rotate left in a 90° angle.
 void turnLeft(){
     motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
     delay(TURN_LEFT_DURATION);
-    
+    if (orientation == 'n'){
+    orientation = 'o';
+    }
+    else if (orientation == 'o'){orientation = 's';}
+    else if(orientation == 's'){orientation = 'e';}
+    else if(orientation == 'e'){orientation = 'n';}
     stop();
 }
 /*
@@ -68,9 +80,58 @@ This function makes the bot rotate right in a 90° angle.
 void turnRight(){
     motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
     delay(TURN_RIGHT_DURATION);
-    
+    if (orientation == 'n'){
+    orientation = 'e';
+    }
+    else if (orientation == 'e'){orientation = 's';}
+    else if(orientation == 's'){orientation = 'o';}
+    else if(orientation == 'o'){orientation = 'n';}
     stop();
 }
 
+/*
+goNext,
+This function makes the robot go from one square to the other.
+@param x: x current position.
+@param y: y current position.
+@param w: x next square position.
+@param z: y next square position.
+*/
+char goNext(int x,int y,int w,int z){
 
+    if (x == w && y < z){
+        if (orientation == 'n'){
+        return 'f';
+        }
+    else if (orientation == 'o'){return 'r';}
+    else if(orientation == 's'){return 'b';}
+    else if(orientation == 'e'){return 'l';}
+    }
+    if (x == w && y > z){
+        if (orientation == 'n'){
+        return 'b';}
+    else if (orientation == 'o'){return 'l';}
+    else if(orientation == 's'){return 'f';}
+    else if(orientation == 'e'){return 'r';}
+    }
+
+    if (x < w && y == z){
+        if (orientation == 'n'){
+        return 'l';}
+    else if (orientation == 'o'){return 'f';}
+    else if(orientation == 's'){return 'r';}
+    else if(orientation == 'e'){return 'b';}
+    }
+    if (x > w && y == z){
+        if (orientation == 'n'){
+        return 'r';}
+    else if (orientation == 'o'){return 'b';}
+    else if(orientation == 's'){return 'l';}
+    else if(orientation == 'e'){return 'f';}
+    }
+    if ((x == w && y == z) || (x !=w && y != z)){
+        return 'e';
+    }
+
+}
 
